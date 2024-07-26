f40df89e5b2151ddf6ed1261ab5a14a3d80d8be1
leighchristiegov <leigh.christie@digital.cabinet-office.gov.uk>
2024-07-26 09:54:40 +0100

    intial commit

---
+++ app/routes.js
@@ -0,0 +1,20 @@
+//
+// For guidance on how to create routes see:
+// https://prototype-kit.service.gov.uk/docs/create-routes
+//
+
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+// BASELINE ROUTES
+
+// pull in support routes
+router.use('/', require('./views/baseline/routes/routes-support.js'))
+
+// pull in auth routes
+router.use('/', require('./views/baseline/routes/routes-auth.js'))
+
+// pull in pyi routes
+router.use('/', require('./views/baseline/routes/routes-pyi.js'))
+
+module.exports = router;
