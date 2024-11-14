65f79d2c2f98d55c22428f9ae8961adce33f1b4b
leighchristiegov <leigh.christie@digital.cabinet-office.gov.uk>
2024-11-14 15:15:28 +0000

    coi/6mfc additions and customisation

--- app/views/baseline/routes/routes-kbv.js
+++ app/views/baseline/routes/routes-kbv.js
@@ -11,10 +11,13 @@ function loadJSONFromFile(fileName, path = 'app/data/') {
 	return JSON.parse(jsonFile) // Return JSON as object
 }
 
-// Sets the KBV question routing based on customisation questions
+// Sets the KBV question routing based on customisation questions OR skips KBVs if it's the COI/6MFC journey
 router.post(`${parentDir}/fraud-cri/kbv-question-selection`, function (req, res) {
 	var kbvQuestionSet = req.session.data['kbv']
-	if (kbvQuestionSet == "experian") {
+	var continuityIdentity = req.session.data['continuityIdentity']
+	if (continuityIdentity) {
+		res.redirect('../ipv-core/page-ipv-success')
+	} else if (kbvQuestionSet == "experian") {
 		res.redirect('../kbv-cri/experian-kbv-start')
 	} else if (kbvQuestionSet == "hmrc") {
 		res.redirect('../nino-cri/enter-national-insurance-number')
--- app/views/baseline/routes/routes-service.js
+++ app/views/baseline/routes/routes-service.js
@@ -4,7 +4,7 @@ const parentDir = '/baseline'
 
 // Add your routes here
 router.post(`${parentDir}/service/generate-url`, (req, res) => {
-	const { whichConfidence, whichKBV, hmrcSets, passFailKBV, experianKBV } = req.body; // Get the form data sent with POST
+	const { whichConfidence, whichKBV, hmrcSets, passFailKBV, experianKBV, continuityIdentity } = req.body; // Get the form data sent with POST
 
 	// Construct query strings and filter out '_unchecked' values
 	const buildConfidenceString = Array.isArray(whichConfidence) ? whichConfidence.join(',') : whichConfidence || '';
@@ -13,13 +13,14 @@ router.post(`${parentDir}/service/generate-url`, (req, res) => {
 	const buildExperianKBVString = Array.isArray(experianKBV) ? experianKBV.join(',') : experianKBV || '';
 	const buildHMRCString = filteredHMRC.join(',');
 	const buildPassFailString = Array.isArray(passFailKBV) ? passFailKBV.join(',') : passFailKBV || '';
+	const buildcontinuityIdentity = Array.isArray(continuityIdentity) ? continuityIdentity.join(',') : continuityIdentity || '';
 
 	// Build the user-readable output
-	const buildUserReadout = `${buildConfidenceString} ➔ ${buildKBVString} ➔ ${buildPassFailString} ➔ ${buildExperianKBVString}` + (buildHMRCString ? ` ➔ ${buildHMRCString}` : '');
+	const buildUserReadout = `${buildConfidenceString} ➔ ${buildKBVString} ➔ ${buildPassFailString} ➔ ${buildExperianKBVString} ➔ ${buildcontinuityIdentity}` + (buildHMRCString ? ` ➔ ${buildHMRCString}` : '');
 
 	// Construct the URI conditionally
 	const startURI = req.protocol + '://' + req.get('host');
-	let userURI = `${startURI}${parentDir}/service/service-start?confidence=${buildConfidenceString}&kbv=${buildKBVString}&passFail=${buildPassFailString}&experianKBV=${buildExperianKBVString}`;
+	let userURI = `${startURI}${parentDir}/service/service-start?confidence=${buildConfidenceString}&kbv=${buildKBVString}&passFail=${buildPassFailString}&experianKBV=${buildExperianKBVString}&continuityIdentity=${buildcontinuityIdentity}`;
 	if (buildHMRCString) {
 		userURI += `&hmrcSet=${buildHMRCString}`;
 	}
