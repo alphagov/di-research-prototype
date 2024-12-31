b98814ccfe2d9467ceabf45badf6e7205fd24b00
leighchristiegov <leigh.christie@digital.cabinet-office.gov.uk>
2024-12-31 12:16:06 +0000

    deleting routing for dl auth check

--- app/routes.js
+++ app/routes.js
@@ -40,38 +40,4 @@ router.use('/', require('./views/baseline/routes/routes-bank-account.js'))
 // pull in service (customisation) routes
 router.use('/', require('./views/baseline/routes/routes-service.js'))
 
-// DRIVING LICENCE AUTH CHECK ROUTES
-// pull in support routes
-router.use('/', require('./views/driving-licence-auth/routes/routes-support.js'))
-
-// pull in auth routes
-router.use('/', require('./views/driving-licence-auth/routes/routes-auth.js'))
-
-// pull in ipv core routes
-router.use('/', require('./views/driving-licence-auth/routes/routes-ipv-core.js'))
-
-// pull in driving licence routes
-router.use('/', require('./views/driving-licence-auth/routes/routes-driving-licence.js'))
-
-// pull in address routes
-router.use('/', require('./views/driving-licence-auth/routes/routes-address.js'))
-
-// pull in nino routes
-router.use('/', require('./views/driving-licence-auth/routes/routes-nino.js'))
-
-// pull in kbv routes
-router.use('/', require('./views/driving-licence-auth/routes/routes-kbv.js'))
-
-// pull in f2f routes
-router.use('/', require('./views/driving-licence-auth/routes/routes-f2f.js'))
-
-// pull in CI routes
-router.use('/', require('./views/driving-licence-auth/routes/routes-claimed-identity.js'))
-
-// pull in bank account routes
-router.use('/', require('./views/driving-licence-auth/routes/routes-bank-account.js'))
-
-// pull in service (customisation) routes
-router.use('/', require('./views/driving-licence-auth/routes/routes-service.js'))
-
 module.exports = router;
