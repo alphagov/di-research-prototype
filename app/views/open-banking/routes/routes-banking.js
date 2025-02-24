6903d9a965905bc0b615d51dfbc130bbaa087dbc
LizzieNightingale1 <107249304+LizzieNightingale1@users.noreply.github.com>
2025-02-24 13:57:09 +0000

    New open banking pages

--- app/views/open-banking/routes/routes-banking.js
+++ app/views/open-banking/routes/routes-banking.js
@@ -7,4 +7,14 @@ const postcodeLookup = require("../../../api/postcode-lookup");
 
 
 
+router.post('/banking-cri/country-answer', function(request, response) {
+
+    var country = request.session.data['country']
+    if (country == "How do you want to sign in to your HSBC account?"){
+        response.redirect("/age")
+    } else {
+        response.redirect("/ineligible-country")
+    }
+})
+
 module.exports = router;
